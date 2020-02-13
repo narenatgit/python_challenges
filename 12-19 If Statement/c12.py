d4c1e50bef3c388883ed0c91dafb59a7c79d2950
Naren Reddy <narendar.v.reddy@gmail.com>
2020-02-13 23:23:34 +0530

    Create c12.py

---
+++ 12-19 If Statement/c12.py
@@ -0,0 +1,7 @@
+
+#Ask for two numbers. If the first one is larger than the second, display
+#the second number first and then the first number, otherwise show the first 
+#number first and then the second.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
